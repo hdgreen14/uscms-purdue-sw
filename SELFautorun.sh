17132ef0636a41c66cf69710dade4cc543e75d7d
hdgreen14 <105872251+hdgreen14@users.noreply.github.com>
2023-07-06 14:56:24 -0400

    Update SELFautorun.sh
    fixed the quotes being weird and changed how the time report is found

--- SELFautorun.sh
+++ SELFautorun.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-config="~/sonic/CMSSW_12_5_0_pre4/src/HeterogeneousCore/SonicTriton/data/models/particlenet_AK4_PT/config.pbtxt"  # Replace with your file name
+config="/home/green642/sonic/CMSSW_12_5_0_pre4/src/HeterogeneousCore/SonicTriton/data/models/particlenet_AK4_PT/config.pbtxt"  # Replace with your file name
 
-batchsize=("10" "25" "50" "75")  # Replace with your desired numbers
+batchsize=("10" "25" "50")  # Replace with your desired numbers
 
 cd  ~/sonic/CMSSW_12_5_0_pre4/src/sonic-workflows
-for number in "${batchsize[@]}"; do
+for number in ${batchsize[@]}; do
     sed -i "6 s/\[ [0-9]* \]/[ $number ]/g" "$config" 
-    echo -e 'Preferred Batch size: ${batchsize[@]} \n' >> output.txt
-    cmsRun run.py maxEvents=1000 threads=4 device=gpu tmi=True | tail -n 34 | head -n 25 >> output.txt
+    echo -e 'Preferred Batch size: '${batchsize[@]}' \n' >> output.txt
+    cmsRun run.py maxEvents=1000 threads=4 device=gpu tmi=True  &> tempoutput.txt | sed -n '/TimeReport>/,/^Mem/ { /^Key/ d; p; }' >> output.txt
+   
     echo -e ' \n\n\n' >> output.txt
 done
