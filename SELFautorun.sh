f4f85ea5cd8f420af4de46a4ce45441737d7132b
Hannah <hdgreen@owu.edu>
2023-07-06 15:24:58 -0400

    Changes to be committed: 	modified:   SELFautorun.sh

--- SELFautorun.sh
+++ SELFautorun.sh
@@ -6,11 +6,11 @@ batchsize=("10" "25" "50")  # Replace with your desired numbers
 
 cd  /home/green642/sonic/CMSSW_12_5_0_pre4/src/sonic-workflows
 for number in ${batchsize[@]}; do
-    echo 'Starting with '${batchsize[@]}''
+    echo 'Starting with '${number}''
     sed -i "6 s/\[ [0-9]* \]/[ $number ]/g" "$config" 
-    echo -e 'Preferred Batch size: '${batchsize[@]}' \n' >> output.txt
+    echo -e 'Preferred Batch size: '${number}' \n' >> output.txt
     cmsRun run.py maxEvents=1000 threads=4 device=gpu tmi=True  &> tempoutput.txt | sed -n '/TimeReport>/,/^Mem/ { /^Key/ d; p; }' >> output.txt
    
     echo -e ' \n\n\n' >> output.txt
-    echo 'Done with '${batchsize[@]}''
+    echo 'Done with '${number}''
 done
