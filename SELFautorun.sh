aaf73fd93a7c642128aa6a982726c7846749d88d
Hannah <hdgreen@owu.edu>
2023-07-10 10:07:37 -0400

    Changes to be committed: 	modified:   SELFautorun.sh added the ability to choose a starting file/directory

--- SELFautorun.sh
+++ SELFautorun.sh
@@ -8,16 +8,20 @@ events=1000
 config="/home/green642/sonic/CMSSW_12_5_0_pre4/src/HeterogeneousCore/SonicTriton/data/models/particlenet_AK4_PT/config.pbtxt"  # Replace with your file name
 #defaults
 
+$startdir=$PWD
+$startname="output.txt" 
+
 help(){
     echo "autoRun [options]"
 	echo 
 	echo "Options:"
-	echo "-b          Choose batch sizes to run (Default: (10)) (Input as a list)"
-    echo "-e          Choose how many events per inference (Default: 1000)"
+	echo "-b          Choose preferred batch size (Default: 10)"
+    echo "-e          Choose how many events per inference (Default: 1000)" #when implimenting, add something to check config's max batch size
+    #and then probably output, like, WARNING: max events is less than desired events 
     echo "-h          Print this message and exit"
 	#echo "-d [dir]    Directory containing run.py"
 	echo "-r          Number of runs per batch size"
-   # echo "-o [name]   Choose output file for time report data (default: output.txt)"
+    echo "-o [name]   Choose output file for time report data (default: PWD/output.txt)"
    # echo "-s"         Show output
 	exit $1
 }
@@ -35,6 +39,10 @@ help 0
 r)
 batchloop="$OPTARG"
 ;;
+o)
+$startdir="$OPTARG"
+
+;;
 esac
 done
 
@@ -47,15 +55,16 @@ cd  /home/green642/sonic/CMSSW_12_5_0_pre4/src/sonic-workflows
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 cmsenv
 for number in ${batchsize[@]}; do
-    echo 'Starting with '${number}''
     sed -i "6 s/\[ [0-9]* \]/[ $number ]/g" "$config" #changes the config line from [x] to [batchsize]
    #sed -i (edit the current config file instead of making a copy)
    #"line 6,  substitute/ [ 1 or more 0-9 digits ]/number/global"
    #global as in, do it for everything on the line. might not need this.
-    echo -e 'Preferred Batch size: '${number}' \n' >> output.txt
-    for i in {1..$batchloop}; do
+ 
+    for ((i = 1; i <= $batchloop; i++)); do
+        echo 'Starting with '${number}', run '$i'/'$batchloop''
+       echo -e 'Preferred Batch size: '${number}' || Run '$i' of '$batchloop' \n' >> output.txt
         cmsRun run.py maxEvents=${events} threads=4 device=gpu tmi=True 2>&1| tee tempoutput.txt #the 2>&1 redirects stderr (which has TimeReport) to stdout
-        sed -n '/TimeReport>/,/^Mem/ { /^Key/ d; p; }' tempoutput.txt >> output.txt
+        sed -n '/TimeReport>/,/^Mem/ { /^Key/ d; p; }' tempoutput.txt >> $startdir
         echo "loop '$i' of '$batchloop'"
     done
     echo -e ' \n\n\n' >> output.txt
